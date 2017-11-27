3a0c84e7e0311761290b61e955606ee57e98719a
George Flauta <george.flauta@colorado.edu>
2017-11-26 23:47:45 -0700

    :sparkles: more challenges completed

--- C#/edgeOfTheOcean.cs
+++ C#/edgeOfTheOcean.cs
@@ -7,3 +7,26 @@ int adjacentElementsProduct(int[] inputArray) {
     }
     return result;
 }
+
+int shapeArea(int n) {
+    return (int)(Math.Pow(n, 2) + Math.Pow(n-1, 2));
+}
+
+int makeArrayConsecutive2(int[] statues) {
+    int highest = statues[statues.Length - 1];
+    int lowest = statues[0];
+    Array.Sort(statues);
+    if (statues.Length < 2) {
+        return 0;
+    } else if (statues.Length == 2) {
+        return statues[1] - statues[0] - 1;
+    } else {
+        int result = 0;
+        for (int i = 0; i < statues.Length - 1; i++ ) {
+            if (statues[i] + 1 != statues[i+1]) {
+                result += statues[i+1] - statues[i] - 1;
+            }
+        }
+        return result;
+    }
+}
--- JS/edgeOfTheOcean.js
+++ JS/edgeOfTheOcean.js
@@ -1,10 +1,43 @@
 adjacentElementsProduct = (inputArray) => {
  for (let i = 0; i < inputArray.length; i++) {
-  if (i === 0) {
+  if (i === 0 || inputArray[i] * inputArray[i+1] > result) {
    result = inputArray[i] * inputArray[i+1]
+  }
+ return result
+}
+
+shapeArea = (n) => {
+    return Math.pow(n, 2) + Math.pow(n-1, 2)
+}
+
+
+makeArrayConsecutive2 = (statues) =>  {
+ let result = 0
+ let lowest = 0
+ let highest = 0
+ for ( i = 0; i < statues.length; i++) {
+  if (i === 0) {
+   lowest = statues[i]
+   highest = statues[i]
   } else {
-   if (inputArray[i] * inputArray[i+1] > result) {
-    result = inputArray[i] * inputArray[i+1]
+   if (statues[i] < lowest) {
+    lowest = statues[i]
+   }
+   if (statues[i] > highest) {
+    highest = statues[i]
+   }
+  }
+ }
+ for (j = lowest; j < highest; j++) {
+  for (k = 0; k < statues.length; k++) {
+   if (statues[k] === j) {
+    console.log('returning')
+    break
+   } else {
+    if (k === statues.length - 1) {
+     result += 1
+     console.log(result)
+    }
    }
   }
  }
