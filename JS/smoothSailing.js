dd4094da871bc8ff9865d47dcbd47340d8d1ba76
George Flauta <george.flauta@colorado.edu>
2017-12-08 23:43:38 -0700

    :sparkles: adding more JS

--- JS/smoothSailing.js
+++ JS/smoothSailing.js
@@ -19,3 +19,29 @@ function getLongestNumber(inputArray) {
     }
     return result
 }
+
+function commonCharacterCount(s1, s2) {
+    let array1 = alphabetize(s1)
+    let array2 = alphabetize(s2)
+    let result = 0;
+    let index = -1
+    for (let i = 0; i < array1.length; i++) {
+        let letter = array1[i]
+        for (let j = 0; j < array2.length; j++) {
+            if (letter === array2[j]) {
+                if (j === index) {
+                    continue
+                } else {
+                    result++
+                    index = j
+                    break
+                }
+            }
+        }
+    }
+    return result
+}
+
+function alphabetize(string) {
+    return string.split('').sort()
+}
